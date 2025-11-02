e24bc85a5136c2a4278347f9af83b7d5c1f7cd3a
Axelthony72 <anzouangomisthony@gmail.com>
2025-11-02 08:03:12 +0000

    Ajout des fonctionnalités de login, logout et redirection selon rôle

--- ecommerce_platform/urls.py
+++ ecommerce_platform/urls.py
@@ -27,10 +27,11 @@ urlpatterns = [
 
     # Redirection automatique vers le dashboard
     path('dashboard/', views.redirect_dashboard, name='redirect_dashboard'),
+
+
     
-    #auth
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+   
 
 
     # Redirections selon rôle
